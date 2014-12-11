e3f9a309a7cbb0bd196973d32c506daf82e2cde3
doug <doug@pulse.me>
2014-12-10 20:58:29 -0800

    more deps

--- webapp/project.clj
+++ webapp/project.clj
@@ -5,9 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [hiccup "1.0.5"]
-                 [compojure "1.1.5"]
-                 [ring "1.3.0"]
-                 [com.novemberain/monter "2.0.0"]
+                 [compojure "1.1.9"]
+                 [ring "1.3.1"]
+                 [com.novemberain/monger "2.0.0"]
 ]
   :plugins [[lein-ring "0.8.11"]]
   :main ^:skip-aot webapp.core
