19445c83e3c96e7b75e15081d1d234495c663452
doug <doug@pulse.me>
2014-12-10 20:33:04 -0800

    lein dependencies

--- webapp/project.clj
+++ webapp/project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [hiccup "1.0.3"]
+                 [compojure "1.1.5"]
+                 [ring/ring-jetty-adapter "1.1.8"]
+]
   :main ^:skip-aot webapp.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
