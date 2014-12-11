f63b9eada0fb0991c8a8fbbcddabdfe7bf263b2b
doug <doug@pulse.me>
2014-12-10 23:45:13 -0800

    clean-up home page

--- webapp/src/webapp/core.clj
+++ webapp/src/webapp/core.clj
@@ -17,8 +17,11 @@
 
 (def site-map 
 	(html
-		[:ul "blog"]
-		[:ul (linkfy "dougpuett.com/ff" "fantasy football")]
+		[:h3 "Doug's Website"]
+		[:ul (linkfy "dougpuett.com/microblog" "Micro-Blog")]
+		[:ul (linkfy "dougpuett.com/ff" "Fantasy Football")]
+		[:ul "Full Blog"]
+		[:ul "Poetry Reader"]
 		))
 
 (def ff (html [:h2 "Championship Odds:"][:h3 "Before Week 1:"][:p "What's Fappening 42.2%, Team Freedman 29.3%, Bill and Ted's ExcellentAdv 15.5%, Champ's Smelly Meat 13.0%"][:h3 "After Week 1:"][:p "Team Freedman 49.4%, Champ's Smelly Meat 39.8%, Bill and Ted's ExcellentAdv 5.7%, What's Fappening 5.2%"][:h2 "Individual Matches:"][:h3 "Before Week 1:"][:p "Team Freedman 60.1%, Bill and Ted's ExcellentAdv 39.9%"][:p "What's Fappening 68.6%, Champ's Smelly Meat 31.4%"][:p "Samuel Beckittns 69.6%, Pat Sajak Off 30.4%"][:p "Team buckley 52.5%, I Didn't Know You Were A Cop 47.5%"][:p "Nebraska Cornholers 53.3%, RGIII's iCloud Pics 46.7%"][:p "The Manifest Destinies 82.0%, Team Stanley 18.0%"][:h3 "After Week 1:"][:p "Team Freedman 87.0%, Bill and Ted's ExcellentAdv 13.0%"][:p "Champ's Smelly Meat 90.2%, What's Fappening 9.8%"][:p "Samuel Beckittns 60.2%, Pat Sajak Off 39.8%"][:p "Team buckley 90.2%, I Didn't Know You Were A Cop 9.8%"][:p "Nebraska Cornholers 100.0%"][:p "The Manifest Destinies 95.3%, Team Stanley 4.7%"]))
@@ -31,7 +34,7 @@
 (def microblog (html (map #(vector :p %) posts)))
 
 (defroutes router*
-	(GET "/" request (str "Doug's Website" site-map))
+	(GET "/" request site-map)
 	(GET "/ff" request ff)
 	(GET "/microblog" request microblog)
 	(compojure.route/not-found "Link not found."))
