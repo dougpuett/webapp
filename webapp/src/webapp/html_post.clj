521b40e09918ebad2c370f0dabdb7f8790fa5c1b
doug <doug@pulse.me>
2014-12-13 22:42:47 -0800

    fixed bug with html_post

--- webapp/src/webapp/html_post.clj
+++ webapp/src/webapp/html_post.clj
@@ -6,15 +6,6 @@
 
 (def microblog_header (html [:head [:meta {:charset "utf-8"}]]))
 
-(defn microblog [] 
-	(str 
-		microblog_header 
-		(html [:body {:style "background-color:#99CCFF;background: linear-gradient(to right, #99CCFF, #8AB8E6, #99CCFF);"} 
-			[:div {:style "background-color:#99CCFF;height:0px"}]
-			[:h2 {:style "margin-left:50px;padding-left:15px;background-color:white;width:485px;padding-top:14px;padding-bottom:14px;border-radius:6px"} "Doug's Microblog"]
-			(map html_post (posts))
-			])))
-
 (defn html_post [post] 
 	(html 
 		[:div {:style "background-color:white;width:500px;padding-top:5px;padding-bottom:5px;margin-bottom:4px;margin-left:50px;border-radius:6px"}
@@ -24,3 +15,12 @@
 		[:div {:style "height:5px"}]
 		[:div {:style "margin-left:10px;margin-right:5px"} (:message post)]
 		]))
+
+(defn microblog [] 
+	(str 
+		microblog_header 
+		(html [:body {:style "background-color:#99CCFF;background: linear-gradient(to right, #99CCFF, #8AB8E6, #99CCFF);"} 
+			[:div {:style "background-color:#99CCFF;height:0px"}]
+			[:h2 {:style "margin-left:50px;padding-left:15px;background-color:white;width:485px;padding-top:14px;padding-bottom:14px;border-radius:6px"} "Doug's Microblog"]
+			(map html_post (posts))
+			])))
